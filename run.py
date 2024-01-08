4f7449810eed316271fb103bed38436631737010
Robert E. Campbell Jr <robert.e.campbell.jr@gmail.com>
2024-01-07 20:05:40 -0700

    get_url-by_title now generates the title: link pairs and saves them to a "shelf" called title_page.db

--- run.py
+++ run.py
@@ -25,7 +25,6 @@ def scrape_asura():
   # The new version will parse the link shelve created by get_url_by_title
   # It will look for the chapter elements in the manhwa's main page
   # It will log the most recent chapter
-  series_divs = site_parser(websites, 'div', 'luf')
   # If the most recent chapter is the same as the last one I read as compared to the last read shelve
     # Then ignore it and alert me somehome
   # elif the chapter is the next chapter (e.g. chapter# == last chapter + 1)
@@ -34,7 +33,6 @@ def scrape_asura():
   ### Some times AT chapters are numbered with decimals (e.g. 1.1, 1.2, 1.3, etc.)
   ### I will have to work on logic to compensate for that eventually
   ### As of now, that doesn't happen too often so I will not worry about it for v1.0
-  titles = filter_titles(series_divs, titles_to_find)
   return "Asura Scraped Smee!", 200
 
 if __name__ == '__main__':
